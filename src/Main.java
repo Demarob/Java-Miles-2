05e3d866e2c4ad0f01a03f8e8f20b44dfc8e3b25
Robert <demarulit2@mail.ru>
2022-02-02 17:37:42 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 5000;
+
+        int percent = 5;
+
+        int bonus =  price * 5 / 100;
+
+        System.out.println(bonus);
+
+    }
+}
